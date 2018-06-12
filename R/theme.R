28ea10d04905c869b28e0f5283b8eb4009e95ac8
Constance Neeser <connie@brandseye.com>
2018-06-12 15:17:12 +0200

    License added

--- R/theme.R
+++ R/theme.R
@@ -1,3 +1,23 @@
+# Copyright (c) 2018 BrandsEye
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 LIGHT_GREY  <- "#e9e9e9"  # gridlines, bars and columns, neutral sentiment
 MID_GREY    <- "#c0c0c0"  # table borders, bars and columns if light grey is too lightGrey
 DARK_GREY   <- "#444444"  # All text
