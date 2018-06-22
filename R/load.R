9565db27209c2c76ea30bab6f03a4d3a3b5b9639
Constance Neeser <connie@brandseye.com>
2018-06-22 09:39:17 +0200

    More robust attempts at loading the fonts that we need

--- R/load.R
+++ R/load.R
@@ -1,9 +1,15 @@
 .onLoad <- function(libname, pkgname) {
   if (!"Open Sans Light" %in% extrafont::fonts()) {
-    warn("Open Sans Light is not installed. Please install it.")
+    extrafont::font_import(pattern = "OpenSans")
+    if (!"Open Sans Light" %in% extrafont::fonts()) {
+      warn("Open Sans Light is not installed. Please install it and reload this package.")
+    }
   }
   if (!"Nunito Sans" %in% extrafont::fonts()) {
-    warn("Nunito Sans is not installed. Please install it.")
+    extrafont::font_import(pattern = "NunitoSans")
+    if (!"Nunito Sans" %in% extrafont::fonts()) {
+      warn("Nunito Sans is not installed. Please install it and reload this package.")
+    }
   }
 
   invisible()
